d80149355a084ab1107c7900ed841e49729fd36d
aLLeXUs <mrallexus@gmail.com>
2018-07-18 23:06:12 +0300

    Implemented database user repository methods

--- app/Repository/DatabaseUserRepository.php
+++ app/Repository/DatabaseUserRepository.php
@@ -8,6 +8,6 @@ class DatabaseUserRepository implements Contracts\UserRepository
 {
     public function getById(int $id): ?User
     {
-        // TODO: Implement getById() method.
+        return User::find($id);
     }
 }

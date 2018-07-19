29dd0844b9ee6832cefdeefdf06bce2b18963f09
aLLeXUs <mrallexus@gmail.com>
2018-07-19 11:40:48 +0300

    Implemented wallet service methods

--- app/Service/WalletService.php
+++ app/Service/WalletService.php
@@ -4,23 +4,45 @@ namespace App\Service;
 
 use App\Entity\Money;
 use App\Entity\Wallet;
+use App\Repository\Contracts\MoneyRepository;
+use App\Repository\Contracts\WalletRepository;
 use App\Request\Contracts\CreateWalletRequest;
 use App\Request\Contracts\MoneyRequest;
 
 class WalletService implements Contracts\WalletService
 {
+    private $moneyRepository;
+    private $walletRepository;
+
+    public function __construct(MoneyRepository $moneyRepository, WalletRepository $walletRepository)
+    {
+        $this->moneyRepository = $moneyRepository;
+        $this->walletRepository = $walletRepository;
+    }
+
     public function addWallet(CreateWalletRequest $walletRequest): Wallet
     {
-        // TODO: Implement addWallet() method.
+        $wallet = $this->walletRepository->findByUser($walletRequest->getUserId());
+        if (empty($wallet)) {
+            $wallet = $this->walletRepository->add(new Wallet([
+                'user_id' => $walletRequest->getUserId(),
+            ]));
+        }
+        return $wallet;
     }
 
     public function addMoney(MoneyRequest $moneyRequest): Money
     {
-        // TODO: Implement addMoney() method.
+        $money = $this->moneyRepository->findByWalletAndCurrency($moneyRequest->getWalletId(),
+            $moneyRequest->getCurrencyId());
+        $money->amount += $moneyRequest->getAmount();
+        $this->moneyRepository->save($money);
     }
 
     public function takeMoney(MoneyRequest $moneyRequest): Money
     {
-        // TODO: Implement takeMoney() method.
+        $money = $this->moneyRepository->findByWalletAndCurrency($moneyRequest->getWalletId(),
+            $moneyRequest->getCurrencyId());
+        $money->amount -= $moneyRequest->getAmount();
     }
 }

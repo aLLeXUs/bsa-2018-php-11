77d45496a7fd935e5a52dafc360871b899c2a566
aLLeXUs <mrallexus@gmail.com>
2018-07-19 09:59:40 +0300

    Implemented currency service methods

--- app/Service/CurrencyService.php
+++ app/Service/CurrencyService.php
@@ -3,12 +3,23 @@
 namespace App\Service;
 
 use App\Entity\Currency;
+use App\Repository\Contracts\CurrencyRepository;
 use App\Request\Contracts\AddCurrencyRequest;
 
 class CurrencyService implements Contracts\CurrencyService
 {
+    private $currencyRepository;
+
+    public function __construct(CurrencyRepository $currencyRepository)
+    {
+        $this->currencyRepository = $currencyRepository;
+    }
+
     public function addCurrency(AddCurrencyRequest $currencyRequest): Currency
     {
-        // TODO: Implement addCurrency() method.
+        $currency = new Currency([
+            'name' => $currencyRequest->getName(),
+        ]);
+        $this->currencyRepository->add($currency);
     }
 }

29b6356144a149bedfb0331c5212af2a896a5504
aLLeXUs <mrallexus@gmail.com>
2018-07-19 09:00:26 +0300

    Implemented add currency request methods

--- app/Request/AddCurrencyRequest.php
+++ app/Request/AddCurrencyRequest.php
@@ -2,10 +2,12 @@
 
 namespace App\Request;
 
-class AddCurrencyRequest implements Contracts\AddCurrencyRequest
+use Illuminate\Foundation\Http\FormRequest;
+
+class AddCurrencyRequest extends FormRequest implements Contracts\AddCurrencyRequest
 {
     public function getName(): string
     {
-        // TODO: Implement getName() method.
+        return request()->input('name');
     }
 }

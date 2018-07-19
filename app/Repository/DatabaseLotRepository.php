51efdc2cd7a354657b198d453d9b79de2026153d
aLLeXUs <mrallexus@gmail.com>
2018-07-19 23:05:50 +0300

    Added methods to lot repository

--- app/Repository/DatabaseLotRepository.php
+++ app/Repository/DatabaseLotRepository.php
@@ -3,6 +3,8 @@
 namespace App\Repository;
 
 use App\Entity\Lot;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
 
 class DatabaseLotRepository implements Contracts\LotRepository
 {
@@ -17,6 +19,16 @@ class DatabaseLotRepository implements Contracts\LotRepository
         return Lot::find($id);
     }
 
+    public function isActiveById(int $id): bool
+    {
+        $lot = Lot::find($id);
+        if ($lot->date_time_open->lte(Carbon::now()) && $lot->date_time_close->gt(Carbon::now())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public function findAll()
     {
         return Lot::all();
@@ -25,7 +37,14 @@ class DatabaseLotRepository implements Contracts\LotRepository
     public function findActiveLot(int $userId): ?Lot
     {
         return Lot::where('seller_id', $userId)
-            ->whereDate('date_time_open', '>=', \Carbon::now())
-            ->whereDate('date_time_close', '<', \Carbon::now())->first();
+            ->whereDate('date_time_open', '>=', Carbon::now())
+            ->whereDate('date_time_close', '<', Carbon::now())->first();
+    }
+
+    public function findAllActiveLots(int $userId): Collection
+    {
+        return Lot::where('seller_id', $userId)
+            ->whereDate('date_time_open', '>=', Carbon::now())
+            ->whereDate('date_time_close', '<', Carbon::now())->get();
     }
 }
